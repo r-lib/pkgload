2e4ee5a3b6b531f43e574acd1241e4570d9bcea2
Jim Hester <james.f.hester@gmail.com>
2017-05-01 16:25:31 -0400

    New function is_dev_package
    Determines if a given package has been loaded by `load_all()`.

--- tests/testthat/test-package.R
+++ tests/testthat/test-package.R
@@ -3,8 +3,10 @@ context('package')
 test_that("it can load from outside of package root", {
   expect_false('testHooks' %in% loadedNamespaces())
   load_all(file.path("testHooks"))
+
   expect_true('testHooks' %in% loadedNamespaces())
+  expect_true(is_dev_package("testHooks"))
+
   unload(file.path("testHooks"))
   expect_false('testHooks' %in% loadedNamespaces())
 })
-
