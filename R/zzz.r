8b97605e38f1b0209d302771f806b04e2bcfc6e0
Jim Hester <james.f.hester@gmail.com>
2017-10-23 15:57:36 -0400

    Only use testthat_source_helpers if testthat is installed

--- R/zzz.r
+++ R/zzz.r
@@ -9,7 +9,12 @@
   assign("desc_desc", desc::desc, envir = env)
   assign("desc_desc_get", desc::desc_get, envir = env)
   assign("desc_desc_get_version", desc::desc_get_version, envir = env)
-  assign("testthat_source_test_helpers", testthat::source_test_helpers, envir = env)
+  if (is_installed("testthat")) {
+    assign("testthat_source_test_helpers", testthat::source_test_helpers, envir = env)
+  } else {
+    assign("testthat_source_test_helpers", function(...) TRUE, envir = env)
+  }
+
 
   nms <- environment(onload_assign)$names
   funs <- environment(onload_assign)$funs
