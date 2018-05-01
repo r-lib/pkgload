8af61231643f4ea9c29d2b6eabee025cff19a305
Jim Hester <james.f.hester@gmail.com>
2018-05-01 15:46:30 -0400

    Revert "Don't load helpers until the end of load_all"
    This reverts commit a9184a0c3025b4c4a8cf80ec1dd0baa2598f58c1.

--- R/load.r
+++ R/load.r
@@ -190,6 +190,13 @@ load_all <- function(path = ".", reset = TRUE, recompile = FALSE,
   run_ns_load_actions(package)
   run_user_hook(package, "load")
 
+  # Source test helpers into package environment
+  if (uses_testthat(path) && helpers) {
+    withr_with_envvar(c(NOT_CRAN = "true", DEVTOOLS_LOAD = "true"),
+      testthat_source_test_helpers(find_test_dir(path), env = ns_env(package))
+    )
+  }
+
   # Set up the exports in the namespace metadata (this must happen after
   # the objects are loaded)
   setup_ns_exports(path, export_all, export_imports)
@@ -213,13 +220,6 @@ load_all <- function(path = ".", reset = TRUE, recompile = FALSE,
 
   # Propagate new definitions to namespace imports of loaded packages.
   propagate_ns(package)
-
-  # Source test helpers into package environment
-  if (uses_testthat(path) && helpers) {
-    withr_with_envvar(c(NOT_CRAN = "true", DEVTOOLS_LOAD = "true"),
-      testthat_source_test_helpers(find_test_dir(path), env = ns_env(package))
-    )
-  }
 
   invisible(out)
 }
