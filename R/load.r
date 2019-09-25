01314ce5fe27302b3bf4c0a3c67ab306c30b5e1c
Kirill Müller <krlmlr+r@mailbox.org>
2019-09-25 19:28:00 +0200

    Sort and shorten bullet list, more specific description of mistake

--- R/load.r
+++ R/load.r
@@ -252,24 +252,40 @@ load_all <- function(path = ".", reset = TRUE, compile = NA,
 }
 
 warn_if_conflicts <- function(package, nms1, nms2) {
-  both <- intersect(nms1, nms2)
+  both <- sort(intersect(nms1, nms2))
   if (length(both) > 0) {
+    # Show at most three bullets because they are repetitive
+    # and because the output size is limited
+    MAX_BULLETS <- 3
     header <- cli::rule(
       left = crayon::bold("Conflicts"),
       right = paste0(package, " ", "conflicts"),
-    width = cli::console_width() - 9L)
+      width = cli::console_width() - 9L
+    )
+    both_short <- utils::head(both, MAX_BULLETS)
     bullets <- paste0(collapse = "\n",
       sprintf(
         "%s %s masks %s::%s()",
         crayon::red(cli::symbol$cross),
-        format(crayon::green(paste0(both, "()"))),
+        format(crayon::green(paste0(both_short, "()"))),
         crayon::blue(package),
-        both
+        both_short
       )
     )
+
+    if (length(both) > MAX_BULLETS) {
+      more <- paste0(
+        cli::symbol$ellipsis, " and ",
+        length(both) - MAX_BULLETS, " more\n"
+      )
+    } else {
+      more <- ""
+    }
+
+    # Show all conflicts in the directions
     directions <- crayon::silver(
       paste0(
-        "Did you accidentally source a file rather than using `load_all()`?\n",
+        "Did you accidentally source a file from `R/` rather than using `load_all()`?\n",
         "Run `rm(list = c(", paste0('"', both, '"', collapse = ", "),
         "))` to remove the conflicts."
       )
@@ -277,9 +293,10 @@ warn_if_conflicts <- function(package, nms1, nms2) {
 
     rlang::warn(
       sprintf(
-        "%s\n%s\n\n%s",
+        "%s\n%s\n%s\n%s",
         header,
         bullets,
+        more,
         directions
       ),
       .subclass = "pkgload::conflict"
