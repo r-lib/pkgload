6954ab0bd1b9efecfe5cdfbd2e0c0a299040f095
Lionel Henry <lionel.hry@gmail.com>
2020-11-23 12:07:03 +0100

    Unregister methods consistently

--- R/load.r
+++ R/load.r
@@ -151,6 +151,7 @@ load_all <- function(path = ".", reset = TRUE, compile = NA,
     # safe usage of dangling references.
     if (is_loaded(package)) {
       unload_pkg_env(package)
+      unregister_methods(package)
       unregister_namespace(package)
     }
   }
