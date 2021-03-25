80b15c9f548fef6eb10ac314502f4db9445414a8
Lionel Henry <lionel.hry@gmail.com>
2021-03-25 12:23:45 +0100

    Preserve methods of generics defined in namespace being unloaded
    Closes r-lib/vctrs#1341

--- tests/testthat/testS3removed/R/S3.r
+++ tests/testthat/testS3removed/R/S3.r
@@ -1,5 +1,13 @@
-
 #' @export
 as.character.pkgload_foobar <- function(x, ...) {
+  "registered"
+}
+
+#' @export
+my_generic <- function(x) {
+  UseMethod("my_generic")
+}
+#' @export
+my_generic.pkgload_foobar <- function(x) {
   "registered"
 }
