3d0a0434103e4efeb59ce02d5bafba236f159287
Hadley Wickham <h.wickham@gmail.com>
2021-12-06 10:10:09 -0600

    Take advantage of with_language()

--- tests/testthat/test-po.R
+++ tests/testthat/test-po.R
@@ -4,7 +4,7 @@ test_that("translation domain correctly loaded", {
   load_all(test_path("testTranslations"))
   withr::defer(unload("testTranslations"))
 
-  expect_equal(withr::with_envvar(list(LANGUAGE = "fr"), hello()), "Bonjour")
+  expect_equal(withr::with_language("fr", hello()), "Bonjour")
 })
 
 test_that("modifed translations are correctly reloaded", {
@@ -14,7 +14,7 @@ test_that("modifed translations are correctly reloaded", {
   # Load package and generate translation
   load_all(pkg)
   withr::defer(unload("testTranslations"))
-  withr::with_envvar(list(LANGUAGE = "fr"), hello())
+  withr::with_language("fr", hello())
 
   # Modify .po file
   po_path <- file.path(pkg, "po", "R-fr.po")
@@ -30,5 +30,5 @@ test_that("modifed translations are correctly reloaded", {
 
   # Re-load and re-translate
   load_all(pkg)
-  expect_equal(withr::with_envvar(list(LANGUAGE = "fr"), hello()), "Salut")
+  expect_equal(withr::with_language("fr", hello()), "Salut")
 })
