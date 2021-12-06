169f4f2b2a5b199062ca984c5e9d218af27d4f22
Hadley Wickham <h.wickham@gmail.com>
2021-12-06 10:02:54 -0600

    Test polishing

--- tests/testthat/test-po.R
+++ tests/testthat/test-po.R
@@ -4,12 +4,10 @@ test_that("translation domain correctly loaded", {
   load_all(test_path("testTranslations"))
   withr::defer(unload("testTranslations"))
 
-  withr::local_envvar(LANGUAGE = "fr")
-  expect_equal(hello(), "Bonjour")
+  expect_equal(withr::with_envvar(list(LANGUAGE = "fr"), hello()), "Bonjour")
 })
 
-
-test_that("translations can be changed and reloaded", {
+test_that("modifed translations are correctly reloaded", {
   pkg <- withr::local_tempdir()
   file.copy(dir(test_path("testTranslations"), full.names = TRUE), pkg, recursive = TRUE)
 
@@ -21,7 +19,6 @@ test_that("translations can be changed and reloaded", {
   po <- readLines(po_path)
   po[length(po)] <- "msgstr \"Salut\""
   writeLines(po, po_path)
-
   # Update .mo
   mo_path <- file.path(pkg, "inst/po/fr/LC_MESSAGES/R-testTranslations.mo")
   out <- system(paste0("msgfmt -c --statistics -o", mo_path, " ", po_path), ignore.stderr = TRUE)
