ca012c6aa50cd82871df6da68be2e71df1ee35cc
Hadley Wickham <h.wickham@gmail.com>
2021-12-06 10:01:27 -0600

    Test that changing translation works

--- tests/testthat/test-po.R
+++ tests/testthat/test-po.R
@@ -7,3 +7,28 @@ test_that("translation domain correctly loaded", {
   withr::local_envvar(LANGUAGE = "fr")
   expect_equal(hello(), "Bonjour")
 })
+
+
+test_that("translations can be changed and reloaded", {
+  pkg <- withr::local_tempdir()
+  file.copy(dir(test_path("testTranslations"), full.names = TRUE), pkg, recursive = TRUE)
+
+  load_all(test_path(pkg))
+  withr::defer(unload("testTranslations"))
+
+  # Modify .po file
+  po_path <- file.path(pkg, "po", "R-fr.po")
+  po <- readLines(po_path)
+  po[length(po)] <- "msgstr \"Salut\""
+  writeLines(po, po_path)
+
+  # Update .mo
+  mo_path <- file.path(pkg, "inst/po/fr/LC_MESSAGES/R-testTranslations.mo")
+  out <- system(paste0("msgfmt -c --statistics -o", mo_path, " ", po_path), ignore.stderr = TRUE)
+  if (out != 0) {
+    stop("Failed to run msgfmt")
+  }
+
+  load_all(pkg)
+  expect_equal(withr::with_envvar(list(LANGUAGE = "fr"), hello()), "Salut")
+})
