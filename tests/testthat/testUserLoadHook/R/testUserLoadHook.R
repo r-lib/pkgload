b7caf11add1c9cb5f112b94bd0aed78a3c0708be
Lionel Henry <lionel.hry@gmail.com>
2022-02-03 10:06:26 +0100

    Seal namespace after loading
    Closes #190

--- tests/testthat/testUserLoadHook/R/testUserLoadHook.R
+++ tests/testthat/testUserLoadHook/R/testUserLoadHook.R
@@ -1,7 +1,11 @@
 .onLoad <- function(...) {
+  stopifnot(!environmentIsLocked(asNamespace("testUserLoadHook")))
+
   setHook(
     packageEvent("testUserLoadHookUpstream", "onLoad"),
     function(...) {
+      stopifnot(environmentIsLocked(asNamespace("testUserLoadHookUpstream")))
+
       # The package exports are populated when user onLoad hooks are run
       stopifnot(is.null(testUserLoadHookUpstream::foo()))
 
