5f49227d602f213a87064cb1c0b13e7c3e20bc26
Lionel Henry <lionel.hry@gmail.com>
2024-01-09 17:01:23 +0100

    Skip language tests if set to C locale
    Closes #264

--- tests/testthat/test-po.R
+++ tests/testthat/test-po.R
@@ -1,23 +1,27 @@
 local_load_all_quiet()
 
 test_that("translation domain correctly loaded", {
+  skip_if_c_locale()
+
   load_all(test_path("testTranslations"))
   withr::defer(unload("testTranslations"))
 
-  expect_equal(with_lang("fr_FR", "fr", hello()), "Bonjour")
+  expect_equal(with_language("fr", hello()), "Bonjour")
 
   load_all(test_path("testTranslations"))
   expect_equal(length(temp_po_dirs("testTranslations")), 1)
 })
 
 test_that("modified translations are correctly reloaded", {
+  skip_if_c_locale()
+
   pkg <- withr::local_tempdir()
   file.copy(dir(test_path("testTranslations"), full.names = TRUE), pkg, recursive = TRUE)
 
   # Load package and generate translation
   load_all(pkg)
   withr::defer(unload("testTranslations"))
-  with_lang("fr_FR", "fr", hello())
+  with_language("fr", hello())
 
   # Modify .po file
   po_path <- file.path(pkg, "po", "R-fr.po")
@@ -33,5 +37,5 @@ test_that("modified translations are correctly reloaded", {
 
   # Re-load and re-translate
   load_all(pkg)
-  expect_equal(with_lang("fr_FR", "fr", hello()), "Salut")
+  expect_equal(with_language("fr", hello()), "Salut")
 })
