r-lib/pkgload

commit 842fe9194cf2ef878c557396ef7d69dde6a60ddd

Use relative paths and sort (#308)

R/compilation-db.R
@@ -25,8 +25,8 @@ generate_db <- function(path = ".") {
     return(invisible(NULL))
   }
 
-  files <- build_files(src_path)
-  commands <- build_commands(src_path, package, files, desc)
+  files <- sort(fs::path_rel(build_files(src_path), src_path))
+  commands <- sort(build_commands(src_path, package, files, desc))
   directives <- Map(
     cmd = commands,
     file = files,
@@ -116,7 +116,7 @@ find_source <- function(file) {
     ))
   }
 
-  candidates[[1]]
+  fs::path(dir, candidates[[1]])
 }
 
 build_commands <- function(src_path, package, files, desc) {

commit 089d65e46806116af46bea69cd4f19a214c672b3

fix: path bug introduced by #308

R/compilation-db.R
@@ -69,7 +69,7 @@ build_files <- function(src_path) {
   if (!has_objects) {
     # If the Makevars doesn't define custom objects, just grab all source files
     # in `src`. Same pattern as in `R CMD shlib`.
-    files <- dir(src_path, pattern = "\\.([cfmM]|cc|cpp|f90|f95|mm)$", all.files = TRUE)
+    files <- dir(src_path, pattern = "\\.([cfmM]|cc|cpp|f90|f95|mm)$", all.files = TRUE, full.names = TRUE)
     return(files)
   }
 