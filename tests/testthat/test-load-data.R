7a1f9cb393b217cf7013278212e0d3ef0c75dffa
Michael Chirico <chiricom@google.com>
2026-03-02 23:42:11 +0000

    simplify

--- tests/testthat/test-load-data.R
+++ tests/testthat/test-load-data.R
@@ -1,24 +1,13 @@
-local_load_all_quiet()
-
 test_that("load_data can load .R files that use utils functions in a clean environment", {
-  # We use a subprocess with R_DEFAULT_PACKAGES=NULL to simulate an
-  # environment where 'utils' is not attached.
-  
-  # Ensure the test package and pkgload paths are absolute
   pkg_path <- normalizePath(test_path("testDataUtils"), mustWork = TRUE)
-  pkgload_path <- normalizePath(package_file(), mustWork = TRUE)
   
-  # We use callr::r() to run a subprocess with a controlled environment.
   expect_equal(
     callr::r(
-      function(pkg_path, pkgload_path) {
-        # Load the development version of pkgload, which has our fix
-        pkgload::load_all(pkgload_path)
-        # Load the test package using the fixed pkgload
+      function(pkg_path) {
         pkgload::load_all(pkg_path)
         testDataUtils::myData$a
       },
-      args = list(pkg_path = pkg_path, pkgload_path = pkgload_path),
+      args = list(pkg_path = pkg_path),
       env = c(R_DEFAULT_PACKAGES = "NULL")
     ),
     1L
